6f4d993ca6308159ae1fe9131dced8bac97fd383
lee jin gyeong <jenjennie707@gmail.com>
2023-05-05 17:57:35 +0900

    글씨체 배경 추가

--- app/src/main/java/com/example/blueday4meals/CustomTextOutLineView.java
+++ app/src/main/java/com/example/blueday4meals/CustomTextOutLineView.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.util.AttributeSet;
