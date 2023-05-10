552982a16e0fca52d93ea4090b60e00d00dad05d
Jongmin <wjm7721@naver.com>
2023-05-10 18:12:25 +0900

    로그인 회원가입 DB연동

---
+++ app/src/main/java/com/example/blueday4meals/RegisterType.java
@@ -0,0 +1,15 @@
+package com.example.blueday4meals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_type);
+    }
+}
+
