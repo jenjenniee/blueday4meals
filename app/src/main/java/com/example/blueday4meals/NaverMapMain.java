3a2334b76f5281148c53769eb75c7c535cfec68f
lee jin gyeong <jenjennie707@gmail.com>
2023-05-26 00:53:42 +0900

    지도페이지 가게정보 추가완료

--- app/src/main/java/com/example/blueday4meals/NaverMapMain.java
+++ app/src/main/java/com/example/blueday4meals/NaverMapMain.java
@@ -49,7 +49,7 @@ public class NaverMapMain extends AppCompatActivity implements NaverMap.OnMapCli
         setContentView(R.layout.naver_map);
 
         FragmentManager fm = getSupportFragmentManager();
-        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.map);
         if (mapFragment == null) {
             fm.beginTransaction().add(R.id.map, mapFragment).commit();
         }
@@ -104,8 +104,6 @@ public class NaverMapMain extends AppCompatActivity implements NaverMap.OnMapCli
         naverMap.setLocationSource(locationSource);  //현재위치 표시
         ActivityCompat.requestPermissions(this, PERMISSIONS, LOCATION_PERMISSION_REQUEST_CODE); //현재위치 표시할때 권한 확인
 
-
-
         // 클라이언트 객체 생성
         NaverMapApiInterface naverMapApiInterface = NaverMapRequest.getClient().create(NaverMapApiInterface.class);
         // 응답을 받을 콜백 구현
@@ -117,9 +115,8 @@ public class NaverMapMain extends AppCompatActivity implements NaverMap.OnMapCli
                 naverMapList = response.body(); // naverMapList에 요청에 대한 응답 결과 저장
                 naverMapInfo = naverMapList.data;
 
-
                 // 마커 여러개 찍기
-                for(int i=0; i < naverMapInfo.size(); i++){
+                for (int i = 0; i < naverMapInfo.size(); i++) {
                     Marker[] markers = new Marker[naverMapInfo.size()];
 
                     markers[i] = new Marker();
@@ -130,10 +127,9 @@ public class NaverMapMain extends AppCompatActivity implements NaverMap.OnMapCli
 
                     int finalI = i;
                     markers[i].setOnClickListener(new Overlay.OnClickListener() {
-                        @Override //클릭이벤트
-                        public boolean onClick(@NonNull Overlay overlay)
-                        {
-                            Toast.makeText(getApplication(), "마커" + finalI + "클릭", Toast.LENGTH_SHORT).show();
+                        @Override
+                        public boolean onClick(@NonNull Overlay overlay) {
+                            getClickHandler(finalI);
                             return false;
                         }
                     });
@@ -145,26 +141,26 @@ public class NaverMapMain extends AppCompatActivity implements NaverMap.OnMapCli
 
             }
         });
-
-
-
-
-
-
     }
 
+    private void getClickHandler(int index) {
+        if (naverMapInfo != null && index >= 0 && index < naverMapInfo.size()) {
+            NaverMapData selectedData = naverMapInfo.get(index);
+            String name = selectedData.getfranchisee_name();
+            Toast.makeText(NaverMapMain.this, "가게 이름: " + name, Toast.LENGTH_SHORT).show();
+            //  이름을 활용하여 추가 작업 수행
+        }
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        if(locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
-            if(!locationSource.isActivated()) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) {
                 naverMap.setLocationTrackingMode(LocationTrackingMode.None);
                 return;
             } else {
                 naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
             }
-
         }
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
