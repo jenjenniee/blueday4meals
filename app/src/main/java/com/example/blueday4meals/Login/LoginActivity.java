4c19310d25e9a11156526a21a8ba292802301eff
Bae Gi-Won <bgw1218@gmail.com>
2023-05-30 19:22:20 +0900

    자동로그인 추가

--- app/src/main/java/com/example/blueday4meals/Login/LoginActivity.java
+++ app/src/main/java/com/example/blueday4meals/Login/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.example.blueday4meals.Login;
 
+import android.app.Activity;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.Spanned;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
@@ -15,7 +19,6 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 import com.example.blueday4meals.ChildMainPage;
 import com.example.blueday4meals.Function.CustomButtonOutLineView;
-import com.example.blueday4meals.ParentMainPage;
 import com.example.blueday4meals.R;
 import com.example.blueday4meals.Register.RegisterType;
 
@@ -27,6 +30,7 @@ import java.util.regex.Pattern;
 public class LoginActivity extends AppCompatActivity {
     private EditText et_id, et_pass;
     private RadioButton rb_child, rb_parents;
+    private CheckBox autoLogin;
     private CustomButtonOutLineView btn_login, btn_register;
 
     @Override
@@ -43,6 +47,7 @@ public class LoginActivity extends AppCompatActivity {
         rb_parents = findViewById(R.id.rb_parents);
         btn_login = findViewById(R.id.btn_login);
         btn_register = findViewById(R.id.btn_register);
+        autoLogin = findViewById(R.id.cb_autoLogin);
 
 
         // 회원가입 버튼을 클릭 시 수행
@@ -54,81 +59,112 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        // 로그인 버튼을 클릭 시 수행
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // EditText에 현재 입력되어있는 값을 가져온다.
-                String userID = et_id.getText().toString();
-                String userPass = et_pass.getText().toString();
+        // 자동로그인 처리
+        String userID = et_id.getText().toString();
+        String userPass = et_pass.getText().toString();
 
-                //아동 로그인
-                Response.Listener<String> c_responseListener = null;
-                if (rb_child.isChecked()) {
-                    c_responseListener = new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
-                                JSONObject jsonObject = new JSONObject(response);
-                                boolean success = jsonObject.getBoolean("success");
-                                if (success) { // 로그인에 성공한 경우
-                                    String userID = jsonObject.getString("userID");
-                                    String userPass = jsonObject.getString("userPassword");
+        SharedPreferences auto = getSharedPreferences("autoLogin", Activity.MODE_PRIVATE);
+        userID = auto.getString("userID", null);
+        userPass = auto.getString("userPass", null);
 
-                                    Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(LoginActivity.this, ChildMainPage.class);//Register대신 메인화면.class로 이동시키면 됨
-                                    intent.putExtra("userID", userID);
-                                    intent.putExtra("userPass", userPass);
-                                    startActivity(intent);
-                                } else { // 로그인에 실패한 경우
-                                    Toast.makeText(getApplicationContext(), "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
-                                    return;
+        if(userID != null && userPass != null){
+
+            Toast.makeText(getApplicationContext(), "자동로그인 되었습니다.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, ChildMainPage.class);//Register대신 메인화면.class로 이동시키면 됨
+            intent.putExtra("userID", userID);
+            intent.putExtra("userPass", userPass);
+            startActivity(intent);
+
+        }else{
+
+            // 로그인 버튼을 클릭 시 수행
+            btn_login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // EditText에 현재 입력되어있는 값을 가져온다.
+                    String userID = et_id.getText().toString();
+                    String userPass = et_pass.getText().toString();
+
+                    //아동 로그인
+                    Response.Listener<String> c_responseListener = null;
+                    if (rb_child.isChecked()) {
+                        c_responseListener = new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
+                                    JSONObject jsonObject = new JSONObject(response);
+                                    boolean success = jsonObject.getBoolean("success");
+
+                                    if (success) { // 로그인에 성공한 경우
+
+                                        if(autoLogin.isChecked()) {
+                                            // 자동 로그인 데이터 저장
+                                            SharedPreferences auto = getSharedPreferences("autoLogin", Activity.MODE_PRIVATE);
+                                            SharedPreferences.Editor autoLoginEdit = auto.edit();
+                                            autoLoginEdit.putString("userID", userID);
+                                            autoLoginEdit.putString("userPass", userPass);
+                                            autoLoginEdit.commit();
+                                        }
+
+                                        String userID = jsonObject.getString("userID");
+                                        String userPass = jsonObject.getString("userPassword");
+
+                                        Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(LoginActivity.this, ChildMainPage.class);//Register대신 메인화면.class로 이동시키면 됨
+                                        intent.putExtra("userID", userID);
+                                        intent.putExtra("userPass", userPass);
+                                        startActivity(intent);
+
+                                    } else { // 로그인에 실패한 경우
+                                        Toast.makeText(getApplicationContext(), "아이디 또는 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                                        return;
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
                                 }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
                             }
-                        }
-                    };
-                }
+                        };
+                    }
 
-                //부모 로그인
-                Response.Listener<String> p_responseListener = null;
-                if (rb_parents.isChecked()) {
-                    p_responseListener = new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
-                                JSONObject jsonObject = new JSONObject(response);
-                                boolean success = jsonObject.getBoolean("success");
-                                if (success) { // 로그인에 성공한 경우
-                                    String userID = jsonObject.getString("userID");
-                                    String userPass = jsonObject.getString("userPassword");
+                    //부모 로그인
+                    Response.Listener<String> p_responseListener = null;
+                    if (rb_parents.isChecked()) {
+                        p_responseListener = new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    // TODO : 인코딩 문제때문에 한글 DB인 경우 로그인 불가
+                                    JSONObject jsonObject = new JSONObject(response);
+                                    boolean success = jsonObject.getBoolean("success");
+                                    if (success) { // 로그인에 성공한 경우
+                                        String userID = jsonObject.getString("userID");
+                                        String userPass = jsonObject.getString("userPassword");
 
-                                    Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(LoginActivity.this, ParentMainPage.class);//Register대신 메인화면.class로 이동시키면 됨
-                                    intent.putExtra("userID", userID);
-                                    intent.putExtra("userPass", userPass);
-                                    startActivity(intent);
-                                } else { // 로그인에 실패한 경우
-                                    Toast.makeText(getApplicationContext(), "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
-                                    return;
+                                        Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(LoginActivity.this, ChildMainPage.class);//Register대신 메인화면.class로 이동시키면 됨
+                                        intent.putExtra("userID", userID);
+                                        intent.putExtra("userPass", userPass);
+                                        startActivity(intent);
+                                    } else { // 로그인에 실패한 경우
+                                        Toast.makeText(getApplicationContext(), "아이디 또는 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                                        return;
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
                                 }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
                             }
-                        }
-                    };
-                }
+                        };
+                    }
 
-                LoginRequest loginRequest = new LoginRequest(userID, userPass, c_responseListener);
-                ParentsLoginRequest parentsLoginRequest = new ParentsLoginRequest(userID, userPass, p_responseListener);
-                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
-                queue.add(loginRequest);
-                queue.add(parentsLoginRequest);
-            }
-        });
+                    LoginRequest loginRequest = new LoginRequest(userID, userPass, c_responseListener);
+                    ParentsLoginRequest parentsLoginRequest = new ParentsLoginRequest(userID, userPass, p_responseListener);
+                    RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                    queue.add(loginRequest);
+                    queue.add(parentsLoginRequest);
+                }
+            });
+        }
 
 
     }
